eb2510a216d865db008d7793feb99f7d6b4bc5b3
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-04-21 15:50:34 +0200

    int fertig

---
+++ vadzim/CS-GK-VC-V/DataTypesLib/MyInt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesLib
+{
+    public static class MyInt
+    {
+        public static void ZeigeLiterals()
+        {
+            int myInt = -3;
+            int myInt2 = 0b_0000_0000_0000_0000_0000_0000_0000_1011; // wird bei der Ausgabe dezimal dargestellt
+            Console.WriteLine($"{myInt}, {myInt2}");
+        }
+
+        public static void ZeigeMöglicheWerte()
+        {
+            Console.WriteLine($"int.MaxValue: {int.MaxValue}");
+            Console.WriteLine($"int.MinValue: {int.MinValue}");
+        }
+
+        public static void ZeigeParsing()
+        {
+            string myStr = "3";
+            string myStr2 = "f";
+            int myInt = int.Parse(myStr);
+            int myInt2;
+            bool istMöglich = int.TryParse(myStr2, out myInt2);
+            Console.WriteLine($"myInt: {myInt}, istMöglich: {istMöglich}, myInt2: {myInt2}");
+            // default-Wert bei int ist 0
+        }
+
+        public static void ZeigeCasting()
+        {
+            double kommaZahl = 44.32;
+            int ganzeZahl = (int)kommaZahl;
+            Console.WriteLine($"ganzeZahl: {ganzeZahl}");
+        }
+
+        public static void ZeigeVergleich()
+        {
+            Console.WriteLine($"5 >= 3: {5 >= 3}");
+            // 3 > 5, <, >=, <=
+
+            // Console.WriteLine($"{}");
+        }
+    }
+}
